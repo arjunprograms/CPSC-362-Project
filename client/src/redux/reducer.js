1c97fcdad66b6a660680ddd74dbd63720f8f10f1
Gilbert Topete <gtopete2004@csu.fullerton.edu>
2024-04-12 10:28:42 -0700

    The website works again (yippee!)

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,14 @@
+import {combineReducers} from "@reduxjs/toolkit"
+
+import userSlice from "./userSlice"
+import themeSlice from "./theme"
+import postSlice from "./postSlice"
+
+const rootReducer = combineReducers({
+
+    user: userSlice,
+    theme: themeSlice,
+    posts: postSlice,
+});
+
+export  {rootReducer};
