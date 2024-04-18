988848b0678bd7b56cdea75c9930d4a5bb0cfa7a
Gilbert Topete <gtopete2004@csu.fullerton.edu>
2024-04-18 14:56:55 -0700

    Halfway done with Friends features

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { ProfileCard, TopBar, FriendsCard } from '../components';
+import { friends, friendRequest, requests } from '../assets/data';
+import { Link } from 'react-router-dom';
+import { NoProfile } from '../assets';
 
 function Home() {
-  const {user} = useSelector(state=> state.user)
+  const {user} = useSelector(state=> state.user);
+  const [friendRequest, setFriendRequest] = useState(requests);
+  const [suggestedFriends, setSuggestedFriends] = useState(friendRequest);
+
   return <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden'>
     <TopBar />
 
@@ -14,9 +20,49 @@ function Home() {
         <FriendsCard friends={user?.friends}/>
       </div>
       {/* Center */}
-      <div></div>
+      <div className='flex-1 h-full bg-primary px-4 flex flex-col gap-6 overflow-y-auto'></div>
       {/* Right */}
-      <div></div>
+      <div className='hidden w-1/4 h-full lg:flex flex-col gap-8 overflow-y-auto'>
+        {/* Friend Request */}
+        <div className='w-full bg-primary shadow-sm rounded-lg px-6 py-5'>
+          <div className='flex items-center justify-between text-xl text-ascent-1 pb-2 border-b border-[#66666645]'>
+            <span>Friend Request</span>
+            <span>{friendRequest?.length}</span>
+          </div>
+
+          <div className='w-full flex flex-col gap-4 pt-4'>
+            {friendRequest?.map(({_id, requestFrom: from}) => (
+              <div 
+                key={_id}
+                className='flex items-center justify-between'
+              >
+                <Link 
+                  to={"/profile/" + from._id}
+                  className='w-full flex gap-4 items-center cursor-pointer'
+                >
+                  <img 
+                    src={from?.profileUrl ?? NoProfile} 
+                    alt={from?.firstName}
+                    className='w-10 h-10 object-cover rounded-full'
+                  />
+
+                  <div className='flex-1'>
+                    <p className='text-base font-medium text-ascent-1'>
+                      {from?.firstName} {from?.lastName}
+                    </p>
+                    <span className='text-sm text-ascent-2'>
+                      {from?.profession ?? "No Profession"}
+                    </span>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Suggested Friends */}
+        <div></div>
+      </div>
     </div>
   </div>
 }
