f673cae66f22ed861cf94b94bae680f43f917b38
Gilbert Topete <gtopete2004@csu.fullerton.edu>
2024-04-22 21:11:22 -0700

    Finished "Friend Request" and "Suggested Friends" sections

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { ProfileCard, TopBar, FriendsCard } from '../components';
-import { friends, friendRequest, requests } from '../assets/data';
+import { ProfileCard, TopBar, FriendsCard, CustomButton } from '../components';
+import { friends, friendRequest, requests, suggest } from '../assets/data';
 import { Link } from 'react-router-dom';
 import { NoProfile } from '../assets';
+import { BsPersonFillAdd } from 'react-icons/bs';
 
 function Home() {
   const {user} = useSelector(state=> state.user);
   const [friendRequest, setFriendRequest] = useState(requests);
-  const [suggestedFriends, setSuggestedFriends] = useState(friendRequest);
+  const [suggestedFriends, setSuggestedFriends] = useState(suggest);
 
   return <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden'>
     <TopBar />
@@ -55,13 +56,71 @@ function Home() {
                     </span>
                   </div>
                 </Link>
+
+                <div className='flex gap-1'>
+                    <CustomButton 
+                      title='Accept'
+                      containerStyles='bg-[#0444a4] text-xs text-white px-1.5 py-1 rounded-full'
+                    />
+
+                    <CustomButton
+                      title='Deny'
+                      containerStyles='border border-[#666] text-xs text-ascent-1 px-1.5 py-1 rounded-full'
+                    />
+                </div>
               </div>
             ))}
           </div>
         </div>
 
         {/* Suggested Friends */}
-        <div></div>
+        <div className='w-full bg-primary shadow-sm rounded-lg px-5 py-5'>
+            <div className='flex items-center justify-between text-lg text-ascent-1 border-b border-[#66666645]'>
+              <span>Friend Suggestion</span>
+            </div>
+            <div className='w-full flex flex-col gap-4 pt-4'>
+              {
+                suggestedFriends?.map((friend) => (
+                  <div 
+                    className='flex items-center justify-between'
+                    key={friend._id}
+                  >
+                    <Link
+                      to={"/profile/" + friend?._id}
+                      key={friend?._id}
+                      className='w-full flex gap-4 items-center cursor-pointer'
+                    >
+                      <img 
+                        src={friend?.profileUrl ?? NoProfile}
+                        alt={friend?.firstName}
+                        className='w-10 h-10 object-cover rounded-full'
+                      />
+                      <div className='flex-1'>
+                        <p className='text-base font-medium text-ascent-1'>
+                          {friend?.firstName} {friend?.lastName}
+                        </p>
+                        <span className='text-sm text-ascent-2'>
+                          {friend?.profession ?? "No Profession"}
+                        </span>
+                      </div>
+                    </Link>
+
+                    <div className='flex gap-1'>
+                      <button
+                        className='bg-[#0444a430] text-sm text-white p-1 rounded'
+                        onClick={() => {}}
+                      >
+                        <BsPersonFillAdd
+                          size={20}
+                          className='text-[#0f52b6]'
+                        />
+                      </button>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+        </div>
       </div>
     </div>
   </div>
