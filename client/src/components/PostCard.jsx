8724555176e5384677b970306d8c2041ec77bc84
Gilbert Topete <gtopete2004@csu.fullerton.edu>
2024-04-23 18:34:23 -0700

    Continued working on Comments section

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -4,6 +4,71 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { BiSolidLike, BiLike, BiComment } from 'react-icons/bi'
 import { MdOutlineDeleteOutline } from 'react-icons/md'
+import { useForm } from 'react-hook-form'
+import TextInput from './TextInput';
+import Loading from './Loading';
+import CustomButton from './CustomButton';
+/*import { TextInput } from './TextInput'
+import { CustomButton } from './CustomButton'
+import { Loading } from './Loading'*/
+
+const CommentForm = ({user, id, replyAt, getComments}) => {
+    const [loading, setLoading] = useState(false);
+    const [errMsg, setErrMsg] = useState("");
+    const {register, handleSubmit, reset, formState: { errors }, } = useForm({
+        mode: "onChange",
+    });
+    const onSubmit = async(data) => {};
+
+
+    return <form 
+                onSubmit={handleSubmit(onSubmit)}
+                className='w-full border-b border-[#66666645]'
+            >
+                <div className='w-full flex items-center gap-2 py-4'>
+                    <img 
+                        src={user?.profileUrl ?? NoProfile}
+                        alt='User Image'
+                        className='w-10 h-10 rounded-full object-cover'
+                    />
+
+                    <TextInput 
+                        name='comment'
+                        styles='w-full rounded-full py-3'
+                        placeholder={replyAt ? `Reply @${replyAt}` : "Comment on this post"}
+                        register={register('comment', {
+                            required: "Comment cannot be empty",
+                        })}
+                        error={errors.comment ? errors.comment.message : ""}
+                    />
+                </div>
+                
+                {errMsg?.message && (
+                    <span
+                        role='alert'
+                        className={`text-sm ${
+                            errMsg?.status === "failed"
+                            ? "text-[#f64949fe]"
+                            : "text-[#2ba150fe]"
+                        } mt-0.5`}
+                    >
+                        {errMsg?.message}
+                    </span>
+                )}
+
+                <div className='flex items-end justify-end pb-2'>
+                    {loading ? (
+                        <Loading />
+                    ) : (
+                        <CustomButton
+                            title='Submit'
+                            type='submit'
+                            containerStyles='bg-[#0444a4] text-white py-1 px-3 rounded-full font-semibold text-sm'
+                        />
+                    )}
+                </div>
+            </form>;
+}
 
 const PostCard = ({post, user, deletePost, likePost}) => {
     const [showAll, setShowAll] = useState(0);
@@ -117,6 +182,27 @@ const PostCard = ({post, user, deletePost, likePost}) => {
                 </div>
         )}
     </div>
+
+    {/* Comments */}
+    {showComments === post?._id && (
+        <div className='w-full mt-4 border-t border-[#66666645] pt-4'>
+            <CommentForm
+                user={user}
+                id={post?._id}
+                getComments={() => getComments(post?._id)}
+            />
+
+            {loading ? (
+                <Loading />
+            ) : comments?.length > 0 ? (
+                <div></div>
+            ) : (
+                <span className='flex text-sm py-4 text-ascent-2 text-center'>
+                    No Comments, be the first to comment!
+                </span>
+            )}
+        </div>
+    )}
   </div>
 }
 
